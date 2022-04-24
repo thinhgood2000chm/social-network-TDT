eebc9e2d86e8cd936af755778b6af2ba58a40e0c
thinhnx <thinhgood9@gmail.com>
2022-04-24 17:56:47 +0700

    [add]: update password, profile , detail profile

--- library/middleWareAuthen.js
+++ library/middleWareAuthen.js
@@ -5,20 +5,16 @@ const {UNAUTHORIZED, FORBIDDEN, BAD_REQUEST} = require('./constant')
 
 function checkAuthen(req,res, next){
     const headerAuthen = req.headers['authorization']
-    console.log("da vao nef ")
-    console.log(headerAuthen)
     if (headerAuthen == undefined){
         return res.sendStatus(FORBIDDEN)
     }
     const bearerToken = headerAuthen.split(' ')[1]
     // console.log(bearerToken)
     if (bearerToken){
-        console.log('da vao 1')
         jwt.verify(bearerToken, JWT_SECRET,(err, decode)=>{
             if(err){
                 return res.sendStatus(FORBIDDEN)
             }
-            console.log(decode['id'], "jajajaj" )
             req.userId = decode['id']
             next()
 
