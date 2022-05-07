c2ab7debbf3acae15f04609921e18a01de8d846f
thinhnx <thinhgood9@gmail.com>
2022-05-07 14:06:48 +0700

    update

--- api/controllers/notificationCtrl.js
+++ api/controllers/notificationCtrl.js
@@ -3,7 +3,7 @@ const notification = require("../../models/notification")
 const {BAD_REQUEST, NOTIFICATION_NOT_FOUND, CASTERROR,} = require('../../library/constant')
 
 
-const newLocal = exports.getAllNoti = (req, res) => {
+exports.getAllNoti = (req, res) => {
     notification.find({ userId: req.userId, deletedFlag: false }).sort({ createdAt: -1, }).limit(10)
         .then((listNoti) => {
             return res.json(listNoti)
@@ -16,8 +16,7 @@ exports.deleteNoti = (req, res)=>{
     // delete chỗ này được gọi là sorf delete, ko xóa hẳn khỏi data base nhưng sẽ xóa không cho người dùng nhìn thấy được thông qua biên deletedFlag
     // ko xóa hoàn toàn đi o mà chỉ ẩn thông báo đó khỏi người dùng nếu người dùng yêu cầu 
     notification.findOneAndUpdate({userId:req.userId, _id: notificationId}, {deletedFlag: true})
-    .then((data)=>{
-        console.log(data)
+    .then(()=>{
         return res.json({"description": "thông báo đã được ẩn đi"})
     })
     .catch(err=>{
