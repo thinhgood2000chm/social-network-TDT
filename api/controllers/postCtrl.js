20fd508d2a34c5c43d45fb1af62a6f9fce19447c
Lê Thanh Bình <lethanhbinhtdt@gmail.com>
2022-05-08 18:47:41 +0700

    update post controller, user model
    remove post in user model

--- api/controllers/postCtrl.js
+++ api/controllers/postCtrl.js
@@ -1,4 +1,4 @@
-const { BAD_REQUEST, USER_NOT_FOUND, SUCCESS_OK, GET_SOME_ERROR_WHEN_UPDATE, NOT_THING_CHANGE } = require('../../library/constant')
+const { NOT_FOUND, BAD_REQUEST, USER_NOT_FOUND, SUCCESS_OK, GET_SOME_ERROR_WHEN_UPDATE, NOT_THING_CHANGE, POST_NOT_FOUND } = require('../../library/constant')
 
 const { json } = require('express')
 const fs = require('fs');
@@ -6,7 +6,7 @@ const { cloudinary } = require('../../library/cloundinary');
 const getLinkYoutube = require('../../library/getLinkYoutube')
 
 const PostModel = require('../../models/post')
-const AccountModel = require('../../models/user')
+// const AccountModel = require('../../models/user')
 
 
 exports.getPosts = (req, res) => {
@@ -26,7 +26,10 @@ exports.getPost = (req, res) => {
     let postId = req.params.postID
     PostModel.findById(postId)
         .then(post => {
-            res.status(SUCCESS_OK).json({ data: post })
+            if(!post)
+                return res.status(NOT_FOUND).json({ message: POST_NOT_FOUND })
+            
+            return res.status(SUCCESS_OK).json({ data: post })
         })
         .catch(e => {
             return res.status(BAD_REQUEST).json({ message: e.message })
@@ -49,7 +52,8 @@ exports.createPost = async (req, res) => {
     if (postImages) {
         image = []
         await Promise.all(postImages.map(async (file) => {
-            const cloud = await cloudinary.uploader.upload(file.path)
+            const cloud = await cloudinary.uploader.upload(file.path, {folder : userId})
+            cloudinary.uploader.upload()
             image.push(cloud.url)
             // remove temp file in public/upload
             fs.unlinkSync(file.path)
@@ -65,11 +69,11 @@ exports.createPost = async (req, res) => {
     })
     newPost.save()
         .then((data) => {
-            AccountModel.findByIdAndUpdate(userId, { $push: { post: { rootPostId: data._id } } }, { new: true })
-                .then(() => {
-                    return res.status(SUCCESS_OK).json({ data: data })
-                })
-
+            // AccountModel.findByIdAndUpdate(userId, { $push: { post: { rootPostId: data._id } } }, { new: true })
+            //     .then(() => {
+            //         return res.status(SUCCESS_OK).json({ data: data })
+            //     })
+            return res.status(SUCCESS_OK).json({ data: data })
         })
         .catch(e => {
             return res.status(BAD_REQUEST).json({ message: e.message })
@@ -93,7 +97,7 @@ exports.updatePost = async (req, res) => {
     if (postImages) {
         image = []
         await Promise.all(postImages.map(async (file) => {
-            const cloud = await cloudinary.uploader.upload(file.path)
+            const cloud = await cloudinary.uploader.upload(file.path, {folder : userId})
             image.push(cloud.url)
             fs.unlinkSync(file.path)
         }))
@@ -108,6 +112,8 @@ exports.updatePost = async (req, res) => {
 
     PostModel.findOneAndUpdate({ _id: postID, userId: userId }, { $set: newData })
         .then((post) => {
+            if (!post)
+                return res.status(NOT_FOUND).json({ message: POST_NOT_FOUND })
             return res.status(SUCCESS_OK).json({ message: 'Cập nhật thành công!', data: post })
         })
         .catch(e => {
@@ -121,20 +127,30 @@ exports.deletePost = (req, res) => {
     let userId = req.userId
     // delete root post
     PostModel.findOneAndRemove({ _id: postID, userId: userId })
-        .then(data => {
-            // delete share post
-            PostModel.remove({ rootPost: postID})
-                .then(() => {
-                    // delete rootPost in account
-                    AccountModel.findByIdAndUpdate(userId, { $pull: { post: { rootPostId: postID } } })
-                    .then(() => {
-                        // delete sharePost in account
-                        AccountModel.updateMany( {}, { $pull: { post: { sharePostId: postID } } })
-                        .then(() => {
-                            return res.status(SUCCESS_OK).json({ message: 'Xóa thành công!', data: data })
-                        })
+        // .then(data => {
+        //     // delete share post
+        //     PostModel.remove({ rootPost: postID})
+        //         .then(() => {
+        //             // delete rootPost in account
+        //             AccountModel.findByIdAndUpdate(userId, { $pull: { post: { rootPostId: postID } } })
+        //             .then(() => {
+        //                 // delete sharePost in account
+        //                 AccountModel.updateMany( {}, { $pull: { post: { sharePostId: postID } } })
+        //                 .then(() => {
+        //                     return res.status(SUCCESS_OK).json({ message: 'Xóa thành công!', data: data })
+        //                 })
                         
-                    })
+        //             })
+        //         })
+        // })
+        .then(data => {
+            if (!data)
+                return res.status(NOT_FOUND).json({ message: POST_NOT_FOUND })
+
+            // delete share post
+            PostModel.deleteMany({ rootPost: postID})
+                .then((dataShare) => {
+                    return res.status(SUCCESS_OK).json({ message: 'Xóa thành công!', data: data, dataShare: dataShare })
                 })
         })
         .catch(e => {
