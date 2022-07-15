c63228e243638cf74613560aebb4499bad3566f5
Lê Thanh Bình <lethanhbinhtdt@gmail.com>
2022-07-15 21:13:22 +0700

    getAllConversation sort by time and lasted msg

--- models/message.js
+++ models/message.js
@@ -3,14 +3,17 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
     conversationId: {
         type: Schema.Types.ObjectId,
+        required: true,
         ref: 'conversation'
     },
     senderId: {
         type: Schema.Types.ObjectId,
+        required: true,
         ref: 'account'
     },
     text: {
         type: String,
+        required: true
     }
 
 }, { timestamps: true })
